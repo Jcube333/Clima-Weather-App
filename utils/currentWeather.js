4d6e59c9e685ca7f4cbd2cc3b9e34daee5a3abc9
Jaimin <Jaiminjagdish@gmail.com>
2023-01-23 01:29:12 +0530

    Code refactoring

--- utils/currentWeather.js
+++ utils/currentWeather.js
@@ -1,23 +1,29 @@
 import request from "postman-request";
 import { keys } from "../keys.js";
-export const weather = (coordinates, callback) => {
+
+//Using JS object destructuring to avoid writing coodinates.lat and coordinates.lon
+export const weather = ({ lat, lon }, callback) => {
   const url =
     "http://api.weatherstack.com/current?access_key=" +
     keys.weatherKey +
     "&query=" +
-    coordinates.lat +
+    lat +
     "," +
-    coordinates.lon;
-  request({ url: url, json: true }, (error, response) => {
+    lon;
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect with Weather Service", undefined); //bcoz function call has a standard convention of (error,response) ...however,it's optional.
-    } else if (response.body.error) {
-      callback(response.body.error.code, undefined);
+    } else if (body.error) {
+      callback(body.error.code, undefined);
     } else {
-      const body = response.body.current;
+      const current = body.current;
       callback(
         undefined,
-        "It is " + body.temperature + ". It feels like " + body.feelslike + "."
+        "It is " +
+          current.temperature +
+          ". It feels like " +
+          current.feelslike +
+          "."
       );
     }
   });
