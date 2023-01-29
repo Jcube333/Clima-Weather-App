27d3020f30beb38535016f151e8a972d8f64c275
Jaimin <Jaiminjagdish@gmail.com>
2023-01-30 01:08:43 +0530

    Clima: Version-1

--- src/app.js
+++ src/app.js
@@ -1,38 +1,41 @@
 import { geoCode } from "./utils/geoCode.js";
 import { weather } from "./utils/currentWeather.js";
 import express from "express";
+import hbs from "hbs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// if (!process.argv[2]) console.log("Please provide a location");
-// else {
-//   geoCode(process.argv[2], (error, response) => {
-//     if (error) return console.log(error);
+const __filename = fileURLToPath(import.meta.url);
 
-//     weather(response, (error, WeatherResponse) => {
-//       if (error) return console.log(error);
-
-//       console.log(WeatherResponse);
-//     });
-//   });
-// }
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.static("public"));
 
+const viewsPath = path.join(__dirname, "../templates/views");
+
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
+const partialsPath = path.join(__dirname, "../templates/partials");
+hbs.registerPartials(partialsPath);
+
 app.get("", (req, res) => {
-  res.send("Heyyy");
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About page</h1>");
-});
-
-app.get("/help", (req, res) => {
-  res.render("help", {
-    msg: "Need Help. Get Help",
+  res.render("index.hbs", {
+    name: "Jcube333",
   });
 });
+
+// app.get("/about", (req, res) => {
+//   res.send("<h1>About page</h1>");
+// });
+
+// app.get("/help", (req, res) => {
+//   res.render("help", {
+//     msg: "Need Help. Get Help",
+//   });
+// });
 
 app.get("/weather", (req, res) => {
   const location = req.query.location;
@@ -51,6 +54,10 @@ app.get("/weather", (req, res) => {
       res.send({ location, Weather: WeatherResponse });
     });
   });
+});
+
+app.get("*", (req, res) => {
+  res.send("<h1> 404 </h1><p>Page not Found.</p>");
 });
 
 app.listen(3000, () => {
